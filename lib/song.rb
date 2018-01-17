4f13f480cec44efe48ae6fc5433e53e0e815a9f3
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-17 20:53:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
     self.all.detect { |x| x.name == name }
   end
   
-  def self.find_or_create_by_name(song_name)
+  def self.find_or_create_by_name(name)
     self.create_by_name(name) || self.find_by_name(name)
   end
   
